4b105a46e34da3ad28c49e632e7bb2bf55871d8d
Alexandr-1973 <144156771+Alexandr-1973@users.noreply.github.com>
2025-01-29 16:31:42 +0200

    fin1

--- server.py
+++ server.py
@@ -4,12 +4,15 @@ import logging
 import websockets
 import names
 from websockets.exceptions import ConnectionClosedOK
+import aiofile
+import aiopath
 import obligatory_task
 
 logging.basicConfig(level=logging.INFO)
 
 class Server:
     clients = set()
+    log_file = aiopath.AsyncPath("logs.txt")
 
     async def register(self, ws):
         ws.name = names.get_full_name()
@@ -19,6 +22,10 @@ class Server:
     async def unregister(self, ws):
         self.clients.remove(ws)
         logging.info(f'{ws.remote_address} disconnects')
+
+    async def log_to_file(self, message: str):
+        async with aiofile.AIOFile(self.log_file, "a") as file:
+            await file.write(message + "\n")
 
     async def send_to_clients(self, message: str):
         if self.clients:
@@ -38,12 +45,15 @@ class Server:
             if message == "exchange":
                 exchange = await obligatory_task.request(f'https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5')
                 await self.send_to_clients(json.dumps(exchange))
+                await self.log_to_file(f"Command {message} done")
             elif message.split(" ")[0]=="exchange" and len(message.split(" "))>1:
                 exchange_list = await obligatory_task.main(message.split(" "))
                 await self.send_to_clients(json.dumps(exchange_list))
+                await self.log_to_file(f"Command {message} done")
             else:
                 text_message=f"{ws.name}: {message}"
                 await self.send_to_clients(json.dumps(text_message))
+
 
 
 async def main():
